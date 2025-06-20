e0226dbdd6d3bf268ef456098aeff986d7860bbc
Augusto Achá <augustoacha177@gmail.com>
2025-06-20 12:27:38 -0300

    Se sube menu de Javascript

---
+++ scripts.js
@@ -0,0 +1,36 @@
+/**Agregar un elemento li */
+let nuevoLi = document.createElement("li");
+nuevoLi.textContent = "Foro";
+document.querySelector("#ul-id").appendChild(nuevoLi);
+
+/**Obtener la lista ul */
+let items = document.querySelectorAll("ul li");
+
+/**Recorrer la lista items */
+items.forEach((item) => {
+  if (item.textContent === "Home") {
+    //console.log(item);
+    //console.log(item.textContent);
+  } else if (item.textContent === "Productos") {
+    item.innerHTML = '<a href="#">Productos</a>';
+  } else if (item.textContent === "Contáctenos") {
+    item.innerHTML = '<a href ="#">Contáctenos</a>';
+  } else if (item.textContent === "Foro") {
+    item.innerHTML = '<a href="#">Foro</a>';
+  }
+});
+
+/**
+productos.forEach(item => {
+    Condicion o usar if else 
+    if ((items.textContent)=== 'Productos'){
+        //item.textContent = '';
+        items.innerHTML='<a>Productos</a>';
+        //console.log(item.textContent);
+    }
+    else if (item.lastElementChild){
+              //contactenos.innerHTML.set
+              console.log(item.textContent);                      
+    }   
+});
+*/
